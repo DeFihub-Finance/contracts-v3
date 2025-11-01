9af02ebcf8b85422263f46fb96ce687a615877b8
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-10-31 21:16:08 -0300

    refactor: get `HubSwap` from swap helper

--- test/utils/SwapHelper.sol
+++ test/utils/SwapHelper.sol
@@ -6,27 +6,35 @@ import {IQuoter} from "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
 
 import {Path} from "./PathUniswapV3.sol";
 import {Constants} from "./Constants.sol";
-import {RoutePlanner, Plan} from "./RoutePlanner.sol";
+import {HubSwapPlanner} from "./HubSwapPlanner.sol";
+import {HubRouter} from "../../contracts/libraries/HubRouter.sol";
+import {IUniversalRouter} from "../../contracts/interfaces/external/IUniversalRouter.sol";
 
 library SwapHelper {
-    struct EncodeInputParams {
-        address router;
-        address recipient;
+    using HubSwapPlanner for HubRouter.HubSwap;
+
+    struct GetHubSwapParams {
         uint16 slippageBps;
+        address recipient;
         uint amount;
         Path path;
         IERC20 inputToken;
         IERC20 outputToken;
         IQuoter quoter;
+        IUniversalRouter router;
     }
 
     // Router commands
     uint constant WRAP_ETH = 0x0b;
     uint constant V3_SWAP_EXACT_IN = 0x00;
 
-    function encodeExactInput(EncodeInputParams memory params) public returns (bytes memory) {
+    function getHubSwapExactInput(
+        GetHubSwapParams memory params
+    ) public returns (HubRouter.HubSwap memory swap) {
+        swap = HubSwapPlanner.init(params.router);
+
         if (params.inputToken == params.outputToken || params.amount == 0)
-            return new bytes(0);
+            return swap;
 
         uint minOutput = getMinOutput(
             params.amount,
@@ -36,9 +44,7 @@ library SwapHelper {
             params.slippageBps
         );
 
-        Plan memory routePlanner = RoutePlanner.init(params.router);
-
-        routePlanner.addCommand(
+        swap.addCommand(
             V3_SWAP_EXACT_IN,
             abi.encode(
                 params.recipient,
@@ -48,13 +54,15 @@ library SwapHelper {
                 false
             )
         );
-
-        return routePlanner.encode();
     }
 
-    function encodeExactNativeInput(EncodeInputParams memory params) public returns (bytes memory) {
+    function getHubSwapExactInputNative(
+        GetHubSwapParams memory params
+    ) public returns (HubRouter.HubSwap memory swap) {
+        swap = HubSwapPlanner.init(params.router);
+
         if (params.inputToken == params.outputToken || params.amount == 0)
-            return new bytes(0);
+            return swap;
 
         uint minOutput = getMinOutput(
             params.amount,
@@ -64,10 +72,8 @@ library SwapHelper {
             params.slippageBps
         );
 
-        Plan memory routePlanner = RoutePlanner.init(params.router);
-
-        routePlanner.addCommand(WRAP_ETH, abi.encode(params.router, params.amount));
-        routePlanner.addCommand(
+        swap.addCommand(WRAP_ETH, abi.encode(params.router, params.amount));
+        swap.addCommand(
             V3_SWAP_EXACT_IN,
             abi.encode(
                 params.recipient,
@@ -77,8 +83,6 @@ library SwapHelper {
                 false
             )
         );
-
-        return routePlanner.encode();
     }
 
     function getMinOutput(
