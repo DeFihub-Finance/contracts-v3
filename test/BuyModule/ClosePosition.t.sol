f09eb2201266008055ab3abdc754dc53941ca19c
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-11-10 15:07:12 -0300

    test: close buy position tests

---
+++ test/BuyModule/ClosePosition.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+import "forge-std/Test.sol";
+
+import {Balances} from "../utils/Balances.sol";
+import {BuyModuleTestHelpers} from "../shared/BuyModuleTestHelpers.sol";
+import {BuyPositionModule} from "../../contracts/modules/BuyPositionModule.sol";
+import {BasePositionModule} from "../../contracts/abstract/BasePositionModule.sol";
+
+contract ClosePosition is Test, BuyModuleTestHelpers {
+    function setUp() public {
+        deployBaseContracts();
+    }
+
+    function test_fuzz_closePosition(uint[] memory allocatedAmounts) public {
+        vm.assume(allocatedAmounts.length > 0 && allocatedAmounts.length <= 20);
+
+        uint tokenId = _createFuzzyBuyPosition(usdt, allocatedAmounts);
+
+        uint[] memory userBalancesBefore = Balances.getAccountBalances(account0, availableTokens);
+        uint[] memory buyModuleBalancesBefore = Balances.getAccountBalances(address(buyPositionModule), availableTokens);
+
+        vm.startPrank(account0);
+
+        _expectEmitBuyPositionClosedEvent(account0, account0, tokenId);
+        buyPositionModule.closePosition(account0, tokenId, new bytes(0));
+
+        vm.stopPrank();
+
+        uint[] memory userBalancesAfter = Balances.getAccountBalances(account0, availableTokens);
+
+        for (uint i; i < availableTokens.length; ++i) {
+            // Assert user received tokens
+            assertEq(
+                userBalancesAfter[i] - userBalancesBefore[i],
+                buyModuleBalancesBefore[i]
+            );
+        }
+    }
+
+    function test_closePosition_reverts_notOwner() public {
+        uint tokenId = _createBuyPosition(0, usdt, new BuyPositionModule.Investment[](0));
+
+        tokenId += 1; // Ensure tokenId is not owned by account0
+
+        vm.startPrank(account0);
+
+        vm.expectRevert(BasePositionModule.Unauthorized.selector);
+        buyPositionModule.closePosition(account0, tokenId, new bytes(0));
+    }
+}
