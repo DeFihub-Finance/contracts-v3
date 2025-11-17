1c0c865fa524bbc75c090118cc98e6b672cecfc3
RauppRafael <raupperino@gmail.com>
2025-11-17 16:29:49 -0300

    chore: removed duplicate whitespace

--- test/utils/TokenPrices.sol
+++ test/utils/TokenPrices.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.30;
 contract TokenPrices {
     mapping(address => uint) internal _prices;
 
-    function setPrice(address token, uint price) public  {
+    function setPrice(address token, uint price) public {
         _prices[token] = price;
     }
 
