c2283b5d3b33fbccdb62e1cc9a44dd97df263f08
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-11-18 21:24:13 -0300

    feat: add balance mapper helper

--- test/utils/Balances.sol
+++ test/utils/Balances.sol
@@ -1,7 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.30;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import {TestERC20} from "./TestERC20.sol";
+
+struct BalanceMap {
+    string identifier;
+}
+
+using BalanceMapper for BalanceMap global;
+
+library BalanceMapper {
+    function init(
+        string memory _identifier
+    ) internal pure returns (BalanceMap memory map) {
+        return BalanceMap({identifier: _identifier});
+    }
+
+    function computeSlot(
+        string memory identifier,
+        IERC20 token
+    ) internal pure returns (bytes32) {
+        // Use identifier to avoid slot collisions between other maps
+        return keccak256(abi.encodePacked(identifier, token));
+    }
+
+    function add(BalanceMap memory map, IERC20 token, uint amount) internal {
+        bytes32 slot = computeSlot(map.identifier, token);
+
+        assembly ("memory-safe") {
+            let currentBalance := tload(slot)
+            tstore(slot, add(currentBalance, amount))
+        }
+    }
+
+    function get(
+        BalanceMap memory map,
+        IERC20 token
+    ) internal view returns (uint amount) {
+        bytes32 slot = computeSlot(map.identifier, token);
+
+        assembly ("memory-safe") {
+            amount := tload(slot)
+        }
+    }
+}
 
 library Balances {
     function getAccountBalances(
