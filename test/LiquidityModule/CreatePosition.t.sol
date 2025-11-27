d431aec80dfc31fdc0f7e53381af593dfc62ee03
Matheus Pissaia <pissaia.matheus03@gmail.com>
2025-11-27 18:13:11 -0300

    tests: create liquidity position

---
+++ test/LiquidityModule/CreatePosition.t.sol
@@ -0,0 +1,131 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+import "forge-std/Test.sol";
+
+import {TestERC20} from "../utils/TestERC20.sol";
+import {UniswapV3Helper} from "../utils/UniswapV3Helper.sol";
+import {LiquidityTestHelpers, CreateInvestmentParams} from "./LiquidityTestHelpers.sol";
+import {Liquidity} from "../../contracts/products/Liquidity.sol";
+import {UsePosition} from "../../contracts/abstract/UsePosition.sol";
+
+contract CreatePositionTest is Test, LiquidityTestHelpers {
+    function setUp() public {
+        deployBaseContracts();
+    }
+
+    function test_fuzz_createPosition(
+        uint8 rand,
+        CreateInvestmentParams[] memory params
+    ) public {
+        TestERC20 inputToken = availableTokens[rand % availableTokens.length];
+
+        (
+            uint totalAmount,
+            Liquidity.Investment[] memory investments
+        ) = _createLiquidityInvestments(
+            inputToken,
+            _boundCreateInvestmentParams(inputToken, params)
+        );
+
+        uint tokenId = _createLiquidityPosition(totalAmount, inputToken, investments);
+        Liquidity.Position memory position = liquidity.getPositions(tokenId);
+        Liquidity.DexPosition[] memory dexPositions = position.dexPositions;
+
+        assertEq(tokenId, 0);
+        assertEq(liquidity.ownerOf(tokenId), account0);
+        assertEq(dexPositions.length, investments.length);
+        assertEq(position.strategistPerformanceFeeBps, 100);
+
+        for (uint i; i < dexPositions.length; ++i) {
+            Liquidity.Investment memory investment = investments[i];
+            Liquidity.DexPosition memory dexPosition = dexPositions[i];
+
+            assertGt(dexPosition.liquidity, 0);
+
+            (uint amount0, uint amount1) = UniswapV3Helper.getPositionTokenAmounts(
+                dexPosition.lpTokenId,
+                factoryUniV3,
+                dexPosition.positionManager
+            );
+
+            TestERC20 token0 = TestERC20(address(dexPosition.token0));
+            TestERC20 token1 = TestERC20(address(dexPosition.token1));
+
+            // Compare price impact values in USD
+            assertApproxEqRel(
+                token0.amountToUsd(amount0) + token1.amountToUsd(amount1),
+                inputToken.amountToUsd(investment.swapAmount0 + investment.swapAmount1),
+                0.05 ether // 5% price impact tolerance
+            );
+        }
+    }
+
+    function test_createPosition_emptyInvestments() public {
+        uint tokenId = _createLiquidityPosition(0, usdc, new Liquidity.Investment[](0));
+
+        Liquidity.Position memory position = liquidity.getPositions(tokenId);
+
+        assertEq(tokenId, 0);
+        assertEq(position.dexPositions.length, 0);
+        assertEq(liquidity.ownerOf(tokenId), account0);
+    }
+
+    function test_createPosition_reverts_feeTooHigh() public {
+        uint16 performanceFee = 1_501; // 1 bps above max
+
+        vm.startPrank(account0);
+
+        vm.expectRevert(Liquidity.FeeTooHigh.selector);
+
+        liquidity.createPosition(
+            abi.encode(
+                Liquidity.InvestParams({
+                    inputToken: usdc,
+                    inputAmount: 0,
+                    investments: new Liquidity.Investment[](0),
+                    strategistPerformanceFeeBps: performanceFee,
+                    strategy: UsePosition.StrategyIdentifier({
+                        strategist: owner,
+                        externalRef: 1
+                    })
+                })
+            )
+        );
+    }
+
+    function test_createPosition_reverts_inputAmountLessThanTotalAllocations() public {
+        CreateInvestmentParams[] memory investmentParams = new CreateInvestmentParams[](1);
+        
+        (
+            uint inputAmount,
+            Liquidity.Investment[] memory investments
+        ) = _createLiquidityInvestments(
+            usdc,
+            _boundCreateInvestmentParams(usdc, investmentParams)
+        );
+
+        inputAmount -= 1; // Subtract to make it less than total allocations
+
+        _mintAndApprove(inputAmount, usdc, account0, address(liquidity));
+
+        vm.startPrank(account0);
+
+        vm.expectRevert(UsePosition.InvalidAllocatedAmount.selector);
+        liquidity.createPosition(_encodeLiquidityInvestParams(usdc, inputAmount, investments));
+    }
+
+    function test_createPosition_reverts_totalAllocationsLessThanTolerance() public {
+        uint inputAmount = 1 ether;
+
+        _mintAndApprove(inputAmount, usdc, account0, address(liquidity));
+
+        vm.startPrank(account0);
+
+        vm.expectRevert(UsePosition.InvalidAllocatedAmount.selector);
+
+        liquidity.createPosition(
+            _encodeLiquidityInvestParams(usdc, inputAmount, new Liquidity.Investment[](0))
+        );
+    }
+}
